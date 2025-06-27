1493dd519117023e07d0e66d6848348f01863d31
Yvelin-officiel <gerbeaudy@yahoo.fr>
2025-06-27 11:10:29 +0200

    init commit

---
+++ Makefile
@@ -0,0 +1,23 @@
+TARGET_PATH=simeis-server.exe
+all: check test release optimize manual
+
+build:
+	cargo build
+
+release:
+	set RUSTFLAGS=-C code-model=kernel && cargo build --release
+
+optimize:
+	strip target/release/${TARGET_PATH}
+
+manual:
+	typst compile doc/manual.md manuel.pdf
+
+check:
+	cargo check
+
+test:
+	cargo test
+
+clean:
+	cargo clean
---
+++ simeis-server/src/main.rs
@@ -0,0 +1,49 @@
+#![allow(unexpected_cfgs)]
+use ntex::web;
+
+use simeis_data::game::Game;
+
+mod api;
+
+pub type GameState = ntex::web::types::State<Game>;
+
+#[ntex::main]
+async fn main() -> std::io::Result<()> {
+    #[cfg(not(feature = "testing"))]
+    let port = 8080;
+
+    #[cfg(feature = "testing")]
+    let port = 9345;
+
+    env_logger::builder()
+        .parse_default_env()
+        .filter_module("ntex_server", log::LevelFilter::Warn)
+        .filter_module("ntex_io", log::LevelFilter::Warn)
+        .filter_module("ntex_rt", log::LevelFilter::Warn)
+        .filter_module("ntex::http::h1", log::LevelFilter::Warn)
+        .init();
+
+    log::info!("Running on http://0.0.0.0:{port}");
+    let (gamethread, state) = Game::init();
+    let game = state.clone();
+
+    let res = web::HttpServer::new(move || {
+        web::App::new()
+            .wrap(web::middleware::Logger::default())
+            .state(state.clone())
+            .configure(|srv| api::configure(srv))
+    })
+    .stop_runtime()
+    .bind(("0.0.0.0", port))?
+    .run()
+    .await;
+
+    game.stop(gamethread).await;
+    res
+}
+
+#[cfg(feature = "heavy_testing")]
+#[test]
+fn test_heavy_testing() {
+    assert!(false);
+}
